ebe8474a80e48fd8028235640bcc4bad9c0e49ac
shreyansh <shreyanshjain501@gmail.com>
2018-01-12 01:02:48 +0530

    view and adding link feature added

---
+++ addlink.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<?php session_start();
+?>
+<div class="log">
+<a href="index.php" class="common">HOME</a>
+<a href="viewLink.php" class="common" >View added link </a>
+<a href="index.php" class="common">LOGOUT</a>
+<?php 
+ echo "<span class='welcome'> Welcome" . $_SESSION["username"];
+?>
+</div>
+
+<div>
+<form method="POST" action="<?php echo $_SERVER["PHP_SELF"];?>"> 
+<p> Just copy the link and paste below </p>
+<input type="text" class="addlink" name="link" required placeholder="www.linkSaver.com"><br>
+<input type="submit" class="submit" value="Add Link">
+</form>
+</div>
+
+<?php 
+include 'setup.php';
+$link="";
+if($_SERVER["REQUEST_METHOD"]=="POST")
+{
+  $link=$_POST["link"];
+  $user=$_SESSION["username"];
+  $sql="INSERT INTO $user(link)
+       VALUES('$link')";
+  if($conn->query($sql)===TRUE)
+  {
+    echo "added successfully";
+  }
+  else
+  {
+   echo "error adding link ".$conn->error;
+  }
+}
+?>
+</body>
+</html>
---
+++ viewLink.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<?php 
+ session_start();
+?>
+<div class="log">
+<a href="index.php" class="common">HOME</a>
+<a href="addlink.php" class="common">ADD LINK</a>
+<a href="index.php" class="common">LOGOUT</a>
+<?php
+ echo "<span class='welcome'> Welcome  " . $_SESSION["username"];
+?>
+
+</div>
+
+<?php
+ include 'setup.php';
+ $user=$_SESSION["username"];
+ $sql="Select * from $user";
+ $result=$conn->query($sql);
+ if($result->num_rows>0)
+ {?>   
+      <table class="table">
+      <tr> <th>Serial no. </th>
+          <th>Link</th></tr>
+      <?php
+      while($rows=$result->fetch_assoc())
+      { ?>
+         <tr> <td><?php echo $rows["id"];?> </td>
+         <td><a href="<?php echo $rows["link"]; ?>" class="TableLink" target="_blank"><?php echo $rows["link"]; ?></a> </td></tr>
+         <?php
+      } 
+     echo  "</table>";
+ }
+?>
+</body>
+</html> 

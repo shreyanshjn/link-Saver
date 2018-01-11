6a8c89565d81379cde2967cbed1956979589cf5c
shreyansh <shreyanshjain501@gmail.com>
2018-01-12 00:59:19 +0530

    Basic html page created

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<div class="main">
+<a class="home"  href="index.php">HOME</a>
+<a class="signup" href="signup.php">SIGNUP</a>
+<a class="login" href="login.php">LOGIN</a>
+</div>
+
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+ include 'header.php';
+?>
+
+<div>
+<h1>The best place to save all links at one place </h1>
+<h2>Keep seperate links for every member</h2>
+</div>
+
+</body>
+</html>
